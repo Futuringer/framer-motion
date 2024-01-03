9ea5ea4e659b1d6315c6eff33eccdca7d5d28955
Fedorov Vladislav <futuringer.learning@gmail.com>
2024-01-03 19:39:29 +0300

    feat: setup drag functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import MagnetWrapper from "./components/magnet-wrapper";
+import "./index.css";
+import {TwoImage} from './assets/two'
+
 function App() {
   return (
-    <div>
-      START
-    </div>
+    <main className='flex items-center justify-center flex-col h-screen bg-gray-900 w-full'>
+      <div className='grid grid-cols-4 w-3/4'>
+          <MagnetWrapper>
+            <TwoImage className="w-32"/>
+          </MagnetWrapper>
+          <MagnetWrapper>
+            <TwoImage className="w-32"/>
+          </MagnetWrapper>
+      </div>
+    </main>
   );
 }
 
